38a63b77c86dd85a9c8949fad878cf6b4026739b
wish911wish <wish911wish@gmail.com>
2018-12-16 19:26:24 +0900

    Create member model

--- db/migrate/20181216100521_create_groups.rb
+++ db/migrate/20181216100521_create_groups.rb
@@ -1,10 +1,10 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-      t.string :name, null: false
+      t.string      :name, null: false
       t.timestamps
     end
 
-    add_index :groups, :name
+    add_index       :groups, :name
   end
 end
