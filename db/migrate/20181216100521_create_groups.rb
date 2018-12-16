c359863ffc231a8628e56c4e1fce24771436a346
wish911wish <wish911wish@gmail.com>
2018-12-16 19:47:22 +0900

    Correction models

--- db/migrate/20181216100521_create_groups.rb
+++ db/migrate/20181216100521_create_groups.rb
@@ -1,10 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-      t.string      :name, null: false
+      t.string      :name, null: false, index: true, unique: true
       t.timestamps
     end
-
-    add_index       :groups, :name
   end
 end
