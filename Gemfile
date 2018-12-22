6adc6ed3fb36181bf3c0acbf8d4a050d36cdf45c
wish911wish <wish911wish@gmail.com>
2018-12-22 20:54:00 +0900

    Install of Carrierwave

--- Gemfile
+++ Gemfile
@@ -58,3 +58,6 @@ gem "haml-rails", "~> 1.0"
 gem "font-awesome-rails"
 
 gem 'devise'
+
+gem 'carrierwave'
+gem 'mini_magick'
---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,19 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+  process convert: 'jpg'
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+  process resize_to_fit: [800, 800]
+
+  def extension_white_list
+    %W[jpg jpeg gif png]
+  end
+
+  def filename
+    "#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.jpg" if original_filename.present?
+  end
+
+end
