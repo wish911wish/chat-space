4ea39d9941c805a80dae42532413b2eaa09c774f
wish911wish <wish911wish@gmail.com>
2018-12-23 12:39:12 +0900

    Delete unnecessary code in image_uploader.erb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,19 +1,4 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-
-  storage :file
-  process convert: 'jpg'
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-  process resize_to_fit: [800, 800]
-
-  def extension_white_list
-    %W[jpg jpeg gif png]
-  end
-
-  def filename
-    "#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.jpg" if original_filename.present?
-  end
-
+  process resize_to_fit: [300, 300]
 end
