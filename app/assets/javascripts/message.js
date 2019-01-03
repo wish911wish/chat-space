7a750d3ae1fc50b955df54709cd4affc21055587
wish911wish <wish911wish@gmail.com>
2019-01-03 10:38:22 +0900

    Implementation of Asynchronous Communication of message transmission function

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,64 @@
+$(function() {
+
+  function buildHTML(message){
+    var htmlMessageInfo = ""
+    var htmlMessageContent = ""
+    var htmlMessageImage = ""
+    var htmlMessageEndTag = ""
+    var htmlMessage = ""
+    htmlMessageInfo = `
+    <div class="message" message_id="${message.id}">
+      <div class="message__data">
+        <span class="message__user">
+        ${message.name}
+        </span>
+        <span class="message__date">
+        ${message.created_at}
+        </span>
+      </div>`
+    if (message.content){
+      htmlMessageContent = `
+      <div class="message__text">
+      ${message.content}
+      </div>`
+    }
+    if (message.image.url){
+      htmlMessageImage = `
+      <img src="${message.image.url}" >`
+    }
+    htmlMessageEndTag = `
+    </div>`
+    htmlMessage = htmlMessageInfo + htmlMessageContent + htmlMessageImage + htmlMessageEndTag
+    $('.js-message-list').append(htmlMessage);
+  }
+
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr("action")
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(messages){
+      var lastMessageId = $('.message').last().attr("message_id")
+      messages.forEach(function(message){
+        if (lastMessageId < message.id) {
+          buildHTML(message);
+        }
+      })
+      $('.js-message-list').animate({scrollTop: $('.js-message-list')[0].scrollHeight}, 500, 'swing');
+      $('.js-message-content').val('');
+    })
+    .fail(function(){
+      alert('メッセージの送信に失敗しました');
+    })
+    .always(function(){
+      $(".input-area__button").removeAttr("disabled");
+    });
+  })
+});
