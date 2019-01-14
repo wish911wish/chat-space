86a092139104137e7f47af61b934ead6b72a8f1f
wish911wish <wish911wish@gmail.com>
2019-01-14 14:42:41 +0900

    Add "store_dir"

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,11 @@
 class ImageUploader < CarrierWave::Uploader::Base
   storage :fog
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+
   include CarrierWave::MiniMagick
   process resize_to_fit: [300, 300]
 end
